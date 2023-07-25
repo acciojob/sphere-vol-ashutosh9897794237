30e948251819e47fab0c0979bb3cbc3855818a20
ashutosh9897794237 <113291959+ashutosh9897794237@users.noreply.github.com>
2023-07-25 16:13:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function volume_sphere() {
     //Write your code here
-  
+	let radius = document.getElementById('radius');
+	 let volume = document.getElementById('volume');
+  volume = 4/3*3.14*(radius*radius*radius);
+
+calcolate.onclick = function volume(radius) {
+	
+}(radius) {
+	
+}
+return volume;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
